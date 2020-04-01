3211ad816df35ec115310f1f981bf0796f5dd864
Ricardo Alanis <ricardo.alanis@newports.com>
2020-04-01 00:44:17 -0600

    Add new graphs and scenarios

--- app.py
+++ app.py
@@ -10,41 +10,41 @@ import seaborn as sns
 import numpy as np
 from src.scenario import plot_scenario
 
+def scenario_label(value):
+    scenario_relation = {
+        3.5:'Base',
+        4.5:'Pesimista',
+        1.5:'Optimista',
+        1.75:'Fuerte',
+        2.625:'Moderada',
+    }
+    return scenario_relation[value]
+
 st.title('Predicción del modelo SEAIR de la evolución de COVID-19 en Jalisco')
 
-population = st.slider(
+population = st.sidebar.slider(
     label="Población",
-    min_value=0.0,
-    max_value=10000000.0,
-    value=8000000.0,
-    step=100000.0,
+    min_value=800000,
+    max_value=10000000,
+    value=4200000,
+    step=100000,
 )
 
-ro = st.slider(
-    label="Ro",
-    min_value=1.0,
-    max_value=4.0,
-    value=3.5,
-    step=0.5,
+ro = st.sidebar.selectbox(
+    label="Escenario de Propagación por #quedateencasa",
+    options=[3.5, 4.5, 1.5, 1.75, 2.625],
+    format_func=scenario_label,
 )
 
-initial_cases = st.slider(
+initial_cases = st.sidebar.slider(
     label="Casos Iniciales",
     min_value=-0,
-    max_value=50,
+    max_value=70,
     value=32,
     step=1,
 )
 
-exposed_per_case = st.slider(
-    label="Expuestos por caso",
-    min_value=-0,
-    max_value=10,
-    value=4,
-    step=1,
-)
-
-number_of_deaths = st.slider(
+number_of_deaths = st.sidebar.slider(
     label="Muertes Reportadas",
     min_value=-0,
     max_value=10,
@@ -52,19 +52,41 @@ number_of_deaths = st.slider(
     step=1,
 )
 
+recovered = st.sidebar.slider(
+    label="Expuestos por caso",
+    min_value=-0,
+    max_value=10,
+    value=0,
+    step=1,
+)
+
+start_day, end_day = st.sidebar.slider(
+    'Dias con intervención',
+    1, 119, (10, 50)
+)
+duration_days = end_day-start_day
+
+
 initial_conditions = {
     'initial_cases':initial_cases,
-    'exposed_per_case': exposed_per_case,
-    'deaths': number_of_deaths # No Muertos,
+    'exposed_per_case': 4,
+    'deaths': number_of_deaths, # No Muertos,
+    'recovered': recovered,
 }
 
-if st.button("Play"):
+if st.sidebar.button("Generar"):
     st.markdown(f"Población: {population:.1f}")
     st.markdown(f"Ro:  {ro:.1f}")
-    fig = plot_scenario(population,initial_conditions, ro , start_intervention =4, intervention_duration =90 )
+    fig = plot_scenario(
+        population,
+        initial_conditions,
+        ro,
+        start_intervention=start_day,
+        intervention_duration =duration_days
+    )
     st.plotly_chart(fig)
 else:
     st.markdown(
         "Selecciona la localización usando los controles Y pulsa en "
-        "el botón 'Pinta'."
+        "el botón 'Generar'."
     )
--- src/scenario.py
+++ src/scenario.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
-def SEAIR_D(t, population, initial_conditions,  Ro = 4, start_intervention=1, intervention_duration=119):
+def SEAIR_D(t, population, initial_conditions,  Ro, start_intervention=10, intervention_duration=50):
     """
     Esta parte de código es la que implementa las ecuaciones diferenciales.
 
@@ -12,13 +13,14 @@ def SEAIR_D(t, population, initial_conditions,  Ro = 4, start_intervention=1, in
     initial_cases = initial_conditions['initial_cases']
     exposed_per_case = initial_conditions['exposed_per_case']
     deaths = initial_conditions['deaths']
+    recovered = initial_conditions['recovered']
 
     I_o = initial_cases / population  # Tenemos 32 casos
     E_o = (initial_cases*exposed_per_case)/ population # Asumimos 4 expuestos por caso
     A_o = exposed_per_case / population
     D_o = deaths / population # No Muertos
     S_o = (1) - (E_o+I_o+A_o+D_o) # El resto somos suceptibles
-    R_o = 0 # NO hay ningun recuperado
+    R_o = recovered # NO hay ningun recuperado
 
     dt =t[1]- t[0]
 
@@ -26,14 +28,13 @@ def SEAIR_D(t, population, initial_conditions,  Ro = 4, start_intervention=1, in
     gamma = 0.5
     rho = 0.82
     kappa = .016
-    Ro = 4.0
 
     S,E,A,I,R,D = [S_o],[E_o],[E_o],[I_o],[R_o],[D_o]
     for i in t[1:]:
         if (i >= start_intervention and i <=(intervention_duration+start_intervention)):
             beta = Ro*gamma
         else:
-            beta = Ro*gamma
+            beta = 3.5*gamma # Base case Ro
 
 
         St = S[-1] - (beta*S[-1]*I[-1])*dt
@@ -52,12 +53,12 @@ def SEAIR_D(t, population, initial_conditions,  Ro = 4, start_intervention=1, in
     return S,E,A,I,R,D
 
 
-def plot_scenario(population,initial_conditions, Ro =4 , start_intervention =1, intervention_duration =119 ):
+def plot_scenario(population, initial_conditions, Ro, start_intervention =10, intervention_duration =50 ):
 
     dt=.01
-    t = np.arange(0,intervention_duration+dt,dt)
+    evaluation_period = np.arange(0,120,dt)
 
-    S,E,A,I,R,D = SEAIR_D(t, population, initial_conditions, Ro, start_intervention,intervention_duration)
+    S,E,A,I,R,D = SEAIR_D(evaluation_period, population, initial_conditions, Ro, start_intervention,intervention_duration)
 
     E_a = np.array(E)*population
     I_a = np.array(I)*population
@@ -67,7 +68,7 @@ def plot_scenario(population,initial_conditions, Ro =4 , start_intervention =1, 
                       ["Asintomaticos o No reportados"]*np.array(E).shape[0] + ["Muertes"]*np.array(E).shape[0])
 
 
-    days = np.concatenate([t,t,t,t])
+    days = np.concatenate([evaluation_period,evaluation_period,evaluation_period,evaluation_period])
     SEIR_df = pd.DataFrame({
         "Casos": np.concatenate([E_a,I_a,A_a,D_a]),
         "Clase": class_graph,
@@ -76,9 +77,24 @@ def plot_scenario(population,initial_conditions, Ro =4 , start_intervention =1, 
 
     fig = px.line(SEIR_df, x="Dias", y="Casos", color='Clase',color_discrete_sequence=["green", "red", "goldenrod", "blue"], template = "ggplot2")
 
-    fig.update_layout(
-        title=f"Predicción del modelo SEAIR de la evolución de COVID-19 en Jalisco",
+    final_day = start_intervention + intervention_duration
+    SEIR_df_filled = SEIR_df[(SEIR_df['Dias'] >= start_intervention) & (SEIR_df['Dias'] <= final_day)]
+    SEIR_df_filled = SEIR_df_filled[(SEIR_df_filled['Clase'] == 'Expuestos')]
+    days_fill = list(SEIR_df_filled['Dias'])
+    values_fill = list(SEIR_df_filled['Casos'])
 
+    #fig = px.area(SEIR_df_filled, x="Dias", y="Casos", color='Clase',color_discrete_sequence=["green", "red", "goldenrod", "blue"], template = "ggplot2")
+
+    fig.add_trace(go.Scatter(
+        x=days_fill,
+        y=values_fill,
+        fill='tozeroy',
+        fillcolor='#B0B0B0',
+        line_color='blue',
+        showlegend=False
+    ))
+
+    fig.update_layout(
         xaxis_title="Días",
         yaxis_title="Casos Totales",
         )
